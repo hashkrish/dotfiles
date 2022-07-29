f84f65cd7c57d6877a3e9609eebff3df74526ed3
krishnan <krishnan@onlinedegree.iitm.ac.in>
2022-07-30 03:57:37 +0530

    added install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+ln -s .bashrc "$HOME/.bashrc"
+ln -s .vimrc "$HOME/.vimrc"
+ln -s .config "$HOME/.config"
+ln -s .xmonad "$HOME/.xmonad"
+ln -s .tmux.conf "$HOME/.tmux.conf"
+
+
+
