9257a70d20a0a57f9670e4413840b9947e909ed4
krishnan <krishnan@onlinedegree.iitm.ac.in>
2022-08-06 03:26:14 +0530

    install.sh update

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+mv "$HOME/.bashrc" "$HOME/.bashrc_bkp"
+mv "$HOME/.vimrc" "$HOME/.vimrc_bkp"
+mv "$HOME/.config" "$HOME/.config_bkp"
+mv "$HOME/.xmonad" "$HOME/.xmonad_bkp"
+mv "$HOME/.tmux.conf" "$HOME/.tmux.conf_bkp"
+
 ln -s .bashrc "$HOME/.bashrc"
 ln -s .vimrc "$HOME/.vimrc"
 ln -s .config "$HOME/.config"
 ln -s .xmonad "$HOME/.xmonad"
 ln -s .tmux.conf "$HOME/.tmux.conf"
-
-
-
