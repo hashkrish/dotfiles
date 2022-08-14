daa1839cc27ce9b804f3341648c9cdba74b1f106
hashkrish <29514440+hashkrish@users.noreply.github.com>
2022-08-15 01:42:13 +0530

    .bashrc added coc.nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -6,35 +6,46 @@ end
 
 vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
+packer.startup(function(use)
+  use 'wbthomason/packer.nvim' -- Package Manager
   use {
-    'svrana/neosolarized.nvim',
+    'svrana/neosolarized.nvim', -- Theme
     requires = { 'tjdevries/colorbuddy.nvim' }
   }
   use 'kyazdani42/nvim-web-devicons' -- File Icons
-  use 'glepnir/lspsaga.nvim'
-  use 'L3MON4D3/LuaSnip' -- Snippets
   use 'hoob3rt/lualine.nvim' -- Statusline
+  use 'akinsho/nvim-bufferline.lua' -- Vim Line
+  use 'norcalli/nvim-colorizer.lua' -- Preview HEX color
+
+  use 'L3MON4D3/LuaSnip' -- Snippets
+  use 'windwp/nvim-autopairs' -- Auto brackets
+  use 'tpope/vim-surround' -- Surround
+  use 'windwp/nvim-ts-autotag' -- Auto complete end tag
+  use 'dinhhuy258/git.nvim' -- Git
+  use 'lewis6991/gitsigns.nvim' -- Show git status icons
+  use 'nvim-lua/plenary.nvim' -- Common utilities
+  use 'nvim-telescope/telescope.nvim' -- File Viewer
+  use 'nvim-telescope/telescope-file-browser.nvim' -- File browser
+  use 'https://github.com/preservim/tagbar' -- Tagbar for code navigation
+  use 'https://github.com/terryma/vim-multiple-cursors' -- CTRL + N for multiple cursors
+
+  use 'neovim/nvim-lspconfig' -- LSP
+  use 'glepnir/lspsaga.nvim'
   use 'onsails/lspkind-nvim' -- Pictograms
   use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
   use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for newvim's built-in LSP
   use 'hrsh7th/nvim-cmp' -- Completion
-  use 'neovim/nvim-lspconfig' -- LSP
   use {
-    'nvim-treesitter/nvim-treesitter',
+    'nvim-treesitter/nvim-treesitter', -- Highlighter
     run = ':TSUpdate'
   }
   use 'jose-elias-alvarez/null-ls.nvim' -- Prettier
-  use "williamboman/mason.nvim"
+  use {
+    'neoclide/coc.nvim',
+    branch = 'release',
+    run = ':!sudo apt install universal-ctags'
+  }
+  use "williamboman/mason.nvim" -- LSP Manager
   use "williamboman/mason-lspconfig.nvim"
-  use 'windwp/nvim-ts-autotag'
-  use 'windwp/nvim-autopairs'
-  use 'nvim-lua/plenary.nvim' -- Common utilities
-  use 'nvim-telescope/telescope.nvim'
-  use 'nvim-telescope/telescope-file-browser.nvim'
-  use 'akinsho/nvim-bufferline.lua'
-  use 'norcalli/nvim-colorizer.lua'
-  use 'dinhhuy258/git.nvim'
-  use 'lewis6991/gitsigns.nvim'
+
 end)
