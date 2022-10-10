fb09f7da05d4c2e3380511e38addcd8348da53fa
hashkrish <29514440+hashkrish@users.noreply.github.com>
2022-10-11 00:49:48 +0530

    nvim dashboard

---
+++ .config/nvim/after/plugin/dashboard.rc.lua
@@ -0,0 +1,45 @@
+local status, db = pcall(require, "dashboard")
+if (not status) then return end
+
+db.default_banner = {
+    '',
+    '',
+    ' ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗',
+    ' ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║',
+    ' ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║',
+    ' ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║',
+    ' ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║',
+    ' ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝',
+    '',
+    ' [ TIP: To exit Neovim, just power off your computer. ] ',
+    '',
+}
+
+db.custom_center = {
+    { icon = '  ',
+        desc = 'Find recent files                       ',
+        action = 'Telescope oldfiles',
+        shortcut = ';o' },
+    { icon = '  ',
+        desc = 'Find buffer files                       ',
+        action = 'Telescope buffers',
+        shortcut = ';b' },
+    { icon = '  ',
+        desc = 'Find files                              ',
+        action = 'Telescope find_files find_command=rg,--hidden,--files',
+        shortcut = ';f' },
+    { icon = '  ',
+        desc = 'File browser                            ',
+        action = 'Telescope file_browser',
+        shortcut = 'sf' },
+    { icon = '  ',
+        desc = 'Find word                               ',
+        action = 'Telescope live_grep',
+        shortcut = ';r' },
+}
+db.session_directory = "/home/dt/.config/nvim/session"
+--
+-- -- Command
+--
+-- DashboardNewFile  -- if you like use `enew` to create file,Please use this command,it's wrap enew and restore the statsuline and tabline
+-- SessionSave,SessionLoad
