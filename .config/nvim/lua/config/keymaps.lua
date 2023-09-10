55a1c75ceb5b8caed639585983de57edd7403682
hashkrish <29514440+hashkrish@users.noreply.github.com>
2023-09-11 03:30:24 +0530

    keymaps window movements

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -11,3 +11,21 @@ vim.keymap.set("n", "<LEADER>uR", "<Cmd>set norelativenumber<CR>", { desc = "tog
 vim.keymap.set("n", "<LEADER>l", "<Cmd>b#<CR>", { desc = "goto last buffer" })
 vim.keymap.set("n", "<LEADER>L", "<Cmd>Lazy<CR>", { desc = "Lazy" })
 vim.keymap.set("x", "<M-s>", "<Plug>(nvim-surround-visual)", { desc = "surround visual" })
+
+
+vim.keymap.del("n", "H")
+vim.keymap.del("n", "L")
+
+vim.keymap.set("n", "<A-h>", "<Cmd>bp<CR>", { desc = "previous buffer" })
+vim.keymap.set("n", "<A-l>", "<Cmd>bn<CR>", { desc = "next buffer" })
+
+-- vim.keymap.del("n", "<A-j>")
+-- vim.keymap.del("n", "<A-k>")
+--
+-- vim.keymap.set("n", "<A-j>", "<C-w>p", { desc = "previous window" })
+-- vim.keymap.set("n", "<A-k>", "<C-w>w", { desc = "next window" })
+
+vim.keymap.set("n", "<A-J>", "<Cmd>resize -2<CR>", { desc = "resize -2" })
+vim.keymap.set("n", "<A-K>", "<Cmd>resize +2<CR>", { desc = "resize +2" })
+vim.keymap.set("n", "<A-H>", "<Cmd>vertical resize -2<CR>", { desc = "vertical resize -2" })
+vim.keymap.set("n", "<A-L>", "<Cmd>vertical resize +2<CR>", { desc = "vertical resize +2" })
--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,13 +1,51 @@
 return {
   {
     "hrsh7th/nvim-cmp",
-    dependencies = { "hrsh7th/cmp-emoji" },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip",
+      "rafamadriz/friendly-snippets",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
       local cmp = require("cmp")
-      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" } }))
+      local luasnip = require("luasnip")
+
+      require("luasnip/loaders/from_vscode").lazy_load()
+
+      cmp.setup({
+        completion = {
+          completeopt = "menu,menuone,preview,noselect",
+        },
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = {
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.close(),
+          ["<C-y>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          }),
+          ["<CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          }),
+        },
+        sources = cmp.config.sources({
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
     end,
   },
 }
-
--- emacs-like keybindings
